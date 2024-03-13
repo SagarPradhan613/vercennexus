dd5288d3a0a66ffa8065aee09416e68bea789069
RoshanDasan <edroshan2001@gmail.com>
2024-03-13 14:16:47 +0530

    test: deploy16

--- src/app/view/SignInComponent.tsx
+++ src/app/view/SignInComponent.tsx
@@ -15,6 +15,7 @@ import ReferralModal from "./RefferalModal";
 import Button from "../components/Button";
 import Flex from "../components/Flex";
 import useIsMobile from "../hooks/useIsMobile";
+import useIsTab from "../hooks/useIsTab";
 
 const style = {
   position: 'absolute',
@@ -35,40 +36,63 @@ const style = {
 };
 
 const SignIn = () => {
-  const searchParams = useSearchParams();
-  const [isModal, setIsModal] = useState<any>(false);
+  const [viewMore, setViewMore] = useState(false)
+  const isBig = useIsTab()
+  const searchParams = useSearchParams()
+  const [isModal, setIsmodal] = useState<boolean>(false);
   const refId = searchParams.get('refId');
 
   const [referrals, setReferrals] = useState<any>([]);
   const [profile, setProfile] = useState<any>(null);
-  const [web3auth, setWeb3auth] = useState<any>(null);
+  const [web3auth, setWeb3auth] = useState<Web3Auth | null>(null);
   const [provider, setProvider] = useState<any>(null);
   const [errorText, setErrorText] = useState<any>(null);
-  const clientId: any = process.env.NEXT_PUBLIC_WEB3AUTH_CLIENTID;
-  const API_URL: any = process.env.NEXT_PUBLIC_API_URL;
-  const [accessToken, setAccessToken] = useState<any>(localStorage.getItem('access_token'));
-  const [copied, setCopied] = useState<any>(false);
-  const isMobile = useIsMobile();
+  const clientId: any = process.env.NEXT_PUBLIC_WEB3AUTH_CLIENTID
+  const isMobile = useIsMobile()
+  const API_URL = process.env.NEXT_PUBLIC_API_URL
+  const [access_token, setAccessToken] = useState(localStorage.getItem('access_token'));
+  const [copied, setCopied] = useState(false)
 
   useEffect(() => {
+    // setWeb3auth(1)
+    // console.log(web3auth); 
+
     const init = async () => {
       try {
-        const web3authInstance = new Web3Auth({
+        const web3auth = new Web3Auth({
           clientId,
-          web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_MAINNET,
+          web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_MAINNET, // import {WEB3AUTH_NETWORK} from "@web3auth/base";
           chainConfig: {
             chainNamespace: CHAIN_NAMESPACES.EIP155,
             chainId: "0x1",
             rpcTarget: "https://rpc.ankr.com/eth",
           },
+
+          uiConfig: {
+            //   appName: "Nexus",
+            //   mode: "light", // light, dark or auto
+            //   loginMethodsOrder: ["google", "twitter", "facebook", "apple",],
+            //   logoLight: "https://nexusprotocol.s3.eu-north-1.amazonaws.com/NexusImages/Nexus+logo+mark+Dark.svg",
+            //   logoDark: "https://nexusprotocol.s3.eu-north-1.amazonaws.com/NexusImages/Nexus+logo+mark+Dark.svg",
+            //   defaultLanguage: "en", // en, de, ja, ko, zh, es, fr, pt, nl
+            //   loginGridCol: 3,
+            //   primaryButton: "socialLogin", // "externalLogin" | "socialLogin" | "emailLogin"
+          },
         });
+        //   console.log(web3auth);
 
-        await web3authInstance.initModal();
-        setWeb3auth(web3authInstance);
+        await web3auth.initModal();
+        setWeb3auth(web3auth);
 
-        if (web3authInstance.status === "connected") {
-          setProvider(web3authInstance.provider);
-          loginSignup(web3authInstance, web3authInstance.provider);
+        if (web3auth.status === "connected") {
+          setProvider(web3auth.provider);
+          loginSignup(web3auth, web3auth.provider);
+
+          // getUserInfo();
+          // getAccounts();
+        }
+        else {
+          // await web3auth?.connect();
         }
       } catch (error) {
         console.log(error);
@@ -76,68 +100,108 @@ const SignIn = () => {
     };
 
     init();
+
+
   }, []);
 
-  useEffect(() => {
-    if (web3auth) {
-      loginSignup(web3auth, web3auth.provider);
-    }
-  }, [web3auth?.status]);
 
   useEffect(() => {
-    getUser();
-  }, [accessToken]);
+    try {
+
+      if (web3auth) {
+        loginSignup(web3auth, web3auth?.provider);
+      }
+    }
+    catch (e) {
+      console.log(e);
+    }
+
+
+  }, [web3auth?.status])
+
+
+  useEffect(() => {
+    getUser()
+  }, [access_token])
 
   useEffect(() => {
     if (refId) {
-      getReferer();
+      getReferer()
     }
-  }, [refId]);
+  }, [refId])
 
-  const copyToClipboard = (text: string) => {
-    navigator.clipboard.writeText(text);
-    setCopied(true);
-    setTimeout(() => setCopied(false), 10000);
-  };
+  const copy = (text: any) => {
+    navigator.clipboard.writeText(text)
+    setCopied(true)
+    setTimeout(() => { setCopied(false) }, 10000)
+  }
 
   const getReferer = async () => {
+
+
     try {
-      const config = {
+      let config = {
         method: "get",
-        url: `${API_URL}/get/referer?id=${refId}`,
+        url: API_URL + `/get/referer?id=${refId}`,
         maxBodyLength: Infinity,
-        headers: { "Content-Type": "application/json" }
+        headers: {
+          "Content-Type": "application/json"
+        }
       };
 
-      const response = await axios.request(config);
-      if (response.data.status === "OK") {
-        // setProfile(response.data.profile);
-      } else if (response.data.status === "NOT OK") {
-        setErrorText(response.data.message);
-      }
-    } catch (error) {
-      console.log(error);
+      axios
+        .request(config)
+        .then((response) => {
+          if (response.data.status === "OK") {
+            // setProfile(response.data.profile)
+          }
+          if (response.data.status === "NOT OK") {
+            setErrorText(response.data.message)
+          }
+        })
+        .catch((error) => {
+
+          console.log("axios", error);
+          // setStatusText("Internal Error");
+        });
+    } catch (e) {
+      console.log(e);
+
     }
-  };
+  }
 
   const getUser = async () => {
+
+
     try {
-      const config = {
+      let config = {
         method: "get",
-        url: `${API_URL}/get/me?token=${accessToken}`,
+        url: API_URL + `/get/me?token=${access_token}`,
         maxBodyLength: Infinity,
-        headers: { "Content-Type": "application/json" }
+        headers: {
+          "Content-Type": "application/json"
+        }
       };
 
-      const response = await axios.request(config);
-      if (response.data.status === "OK") {
-        setProfile(response.data.profile);
-        setReferrals(response.data.referrals);
-      }
-    } catch (error) {
-      console.log(error);
+      axios
+        .request(config)
+        .then((response) => {
+          if (response.data.status === "OK") {
+            setProfile(response.data.profile)
+            setReferrals(response.data.referrals)
+          }
+        })
+        .catch((error) => {
+
+          console.log("axios", error);
+          // setStatusText("Internal Error");
+        });
+    } catch (e) {
+      console.log(e);
+
     }
-  };
+  }
+
 
   const isConnected = async () => {
     if (!web3auth) {
@@ -148,65 +212,92 @@ const SignIn = () => {
   };
 
   const login = async () => {
-    const isConnectedStatus = await isConnected();
-    if (isConnectedStatus) {
-      loginSignup(web3auth, provider);
+    const _isConnected = await isConnected();
+    // alert(_isConnected)
+    if (_isConnected) {
+      // alert("Already loggedin");
+      loginSignup(web3auth, provider)
       return;
     }
     try {
       const web3authProvider = await web3auth?.connect();
       setProvider(web3authProvider);
-    } catch (error) {
-      console.log(error);
+      // loginSignup(web3auth,web3authProvider)
     }
-  };
+    catch (e) {
+      console.log(e)
+    }
 
-  const loginSignup = async (web3authInstance: Web3Auth, providerInstance: IProvider | null) => {
-    const userInfo = await web3authInstance?.getUserInfo();
-    const ethAddress:any = (await providerInstance?.request({ method: "eth_accounts" })) || "";
+
+  }
+
+  const loginSignup = async (_web3auth: Web3Auth | null, _provider: IProvider | null | undefined) => {
+    const userInfo = await _web3auth?.getUserInfo();
+    // const accounts = await provider?.();
+
+    const eth_address: string = await _provider?.request({ method: "eth_accounts" }) || "";
     if (userInfo) {
+
       try {
-        const config = {
+        let config = {
           method: "post",
-          url: `${API_URL}/login`,
+          url: API_URL + `/login`,
           maxBodyLength: Infinity,
-          headers: { "Content-Type": "application/json" },
+          headers: {
+            "Content-Type": "application/json"
+          },
           data: {
             email: userInfo?.email,
             fullName: userInfo?.name,
-            address: ethAddress[0],
+            address: eth_address[0],
             referredBy: refId || 0
-          }
+          },
         };
 
-        const response = await axios.request(config);
-        if (response.data.status === "OK") {
-          localStorage.setItem('access_token', response.data.access_token);
-          setAccessToken(response.data.access_token);
-          getUser();
-        } else if (response.data.status === "NOT OK") {
-          setErrorText(response.data.message);
-          await web3authInstance?.logout();
-        }
-      } catch (error) {
-        console.log(error);
+        axios
+          .request(config)
+          .then(async (response) => {
+            if (response.data.status === "OK") {
+              localStorage.setItem('access_token', response.data.access_token)
+              setAccessToken(response.data.access_token)
+              getUser()
+            }
+            if (response.data.status === "NOT OK") {
+              setErrorText(response.data.message)
+              await web3auth?.logout();
+
+            }
+          })
+          .catch((error) => {
+
+            console.log("axios", error);
+            // setStatusText("Internal Error");
+          });
+      } catch (e) {
+        console.log(e);
+
       }
     }
-  };
+  }
 
   const logout = async () => {
-    const isConnectedStatus = await isConnected();
-    if (isConnectedStatus) {
+    const _isConnected = await isConnected();
+
+    if (_isConnected) {
       await web3auth?.logout();
       setProvider(null);
-      localStorage.clear();
-      setProfile(false);
+      localStorage.clear()
+      setProfile(false)
+      return;
     }
-  };
+
+
+  }
+
 
   const handleClose = () => {
-    setIsModal(false);
-  };
+    setIsmodal(false)
+  }
 
   return (
 
@@ -305,7 +396,7 @@ const SignIn = () => {
           aria-describedby="modal-modal-description"
         >
           <Box sx={style}>
-            <ReferralModal referrals={referrals} setIsmodal={setIsModal} id={profile?._id} />
+            <ReferralModal referrals={referrals} setIsmodal={setIsmodal} id={profile?._id} />
           </Box>
         </Modal>
       )}
