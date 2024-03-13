1e0c38ab0dae97428a09ccb30ef4c97f5fbc2762
RoshanDasan <edroshan2001@gmail.com>
2024-03-13 16:57:15 +0530

    ready to test

--- src/app/view/landing.jsx
+++ src/app/view/landing.jsx
@@ -57,10 +57,8 @@ const IconButtonWrapper = styled.div`
 `;
 
 function SearchBarFallback() {
-  return <>placeholder</>
+  return <>placeholder</>;
 }
- 
-
 
 const Landing = () => {
   const isMobile = useIsMobile();
@@ -117,105 +115,105 @@ const Landing = () => {
     <div className="gradient-background">
       {!isMobile && <div class="cursor"></div>}
 
-      <Header />
-      <Flex direction={"column"} gap={isTab ? "3rem" : "5rem"}>
-        <Flex
-          justify={isTab ? "center" : "space-between"}
-          items={isMobile ? "center" : "start"}
-          width={"95%"}
-          maxWidth={"1440px"}
-          direction={isTab ? "column" : "row"}
-          m={isTab && !isMobile ? "0px 0px 0px 100px" : "0px"}
-        >
+        <Header />
+        <Flex direction={"column"} gap={isTab ? "3rem" : "5rem"}>
           <Flex
-            direction={"column"}
-            items={"start"}
-            gap={"0px"}
-            height={isTab ? "fit-content" : "100%"}
-            m={isMobile ? "0px 0px 0px 0px" : "0px"}
+            justify={isTab ? "center" : "space-between"}
+            items={isMobile ? "center" : "start"}
+            width={"95%"}
+            maxWidth={"1440px"}
+            direction={isTab ? "column" : "row"}
+            m={isTab && !isMobile ? "0px 0px 0px 100px" : "0px"}
           >
-            <div
-              className={textAnim.length >= 1 ? "swiftup" : "swiftupdisable"}
+            <Flex
+              direction={"column"}
+              items={"start"}
+              gap={"0px"}
+              height={isTab ? "fit-content" : "100%"}
+              m={isMobile ? "0px 0px 0px 0px" : "0px"}
             >
-              <Heading lineHeight={"90px"}>Empowering</Heading>
-            </div>
-            <div
-              className={textAnim.length >= 2 ? "swiftup" : "swiftupdisable"}
+              <div
+                className={textAnim.length >= 1 ? "swiftup" : "swiftupdisable"}
+              >
+                <Heading lineHeight={"90px"}>Empowering</Heading>
+              </div>
+              <div
+                className={textAnim.length >= 2 ? "swiftup" : "swiftupdisable"}
+              >
+                <Heading lineHeight={"90px"}>Innovation</Heading>
+              </div>
+              <div
+                className={textAnim.length >= 3 ? "swiftup" : "swiftupdisable"}
+              >
+                <Flex height={"fit-content"} gap={isMobile ? "1rem" : "3rem"}>
+                  <Heading lineHeight={"90px"}>Futures</Heading>
+                  <IconButtonWrapper>
+                    <IconButton
+                      width={isMobile ? "35px" : "65px"}
+                      icon={isMobile ? "25px" : "30px"}
+                      color={COLORS.blue}
+                      hoverbg={COLORS.black}
+                      hovercolor={COLORS.white}
+                    >
+                      <FaArrowRight />
+                    </IconButton>
+                  </IconButtonWrapper>
+                </Flex>
+              </div>
+            </Flex>
+            <Flex
+              direction={"column"}
+              height={isTab ? "fit-content" : "100%"}
+              items={"start"}
+              m={isMobile ? "0px 0px 0px 40px" : "45px 0px 0px 0px"}
             >
-              <Heading lineHeight={"90px"}>Innovation</Heading>
-            </div>
-            <div
-              className={textAnim.length >= 3 ? "swiftup" : "swiftupdisable"}
-            >
-              <Flex height={"fit-content"} gap={isMobile ? "1rem" : "3rem"}>
-                <Heading lineHeight={"90px"}>Futures</Heading>
-                <IconButtonWrapper>
-                  <IconButton
-                    width={isMobile ? "35px" : "65px"}
-                    icon={isMobile ? "25px" : "30px"}
-                    color={COLORS.blue}
-                    hoverbg={COLORS.black}
-                    hovercolor={COLORS.white}
-                  >
-                    <FaArrowRight />
-                  </IconButton>
-                </IconButtonWrapper>
-              </Flex>
-            </div>
+              <Text
+                align={"start"}
+                maxWidth={isMobile ? "275px" : "500px"}
+                size={"25px"}
+                color={COLORS.light}
+                m={"0px 0px 30px 0px"}
+              >
+                We provide straight forward tools that maximize financial
+                opportunities. forward tools
+              </Text>
+              <Suspense fallback={<SearchBarFallback />}>
+                <SignInComponent />
+              </Suspense>
+            </Flex>
           </Flex>
           <Flex
-            direction={"column"}
-            height={isTab ? "fit-content" : "100%"}
-            items={"start"}
-            m={isMobile ? "0px 0px 0px 40px" : "45px 0px 0px 0px"}
+            justify={"space-between"}
+            items={isBig ? "center" : "start"}
+            width={"100%"}
+            maxWidth={"1440px"}
+            direction={isBig ? "column" : "row"}
           >
-            <Text
-              align={"start"}
-              maxWidth={isMobile ? "275px" : "500px"}
-              size={"25px"}
-              color={COLORS.light}
-            >
-              We provide straight forward tools that maximize financial
-              opportunities. forward tools
-            </Text>
-        <Suspense fallback={<SearchBarFallback />}>
-            <SignInComponent />
-        </Suspense>
-      
-       
-          </Flex>
-        </Flex>
-        <Flex
-          justify={"space-between"}
-          items={isBig ? "center" : "start"}
-          width={"100%"}
-          maxWidth={"1440px"}
-          direction={isBig ? "column" : "row"}
-        >
-          {isBig ? (
-            <SliderCarousal>
-              <Card title={"Engage"} />
-              <Card title={"Snapshot"} />
-              <Card title={"Lottery"} />
-              <Card title={"Contribute"} isTail={false} />
-            </SliderCarousal>
-          ) : (
-            <MoveFromLeftWrapper>
-              <Flex gap={"0px"}>
+            {isBig ? (
+              <SliderCarousal>
                 <Card title={"Engage"} />
                 <Card title={"Snapshot"} />
                 <Card title={"Lottery"} />
                 <Card title={"Contribute"} isTail={false} />
-              </Flex>
-            </MoveFromLeftWrapper>
-          )}
+              </SliderCarousal>
+            ) : (
+              <MoveFromLeftWrapper>
+                <Flex gap={"0px"}>
+                  <Card title={"Engage"} />
+                  <Card title={"Snapshot"} />
+                  <Card title={"Lottery"} />
+                  <Card title={"Contribute"} isTail={false} />
+                </Flex>
+              </MoveFromLeftWrapper>
+            )}
 
-          <Flex width={"100%"}>
-            <BlueCard />
+            <Flex width={"100%"}>
+              <BlueCard />
+            </Flex>
           </Flex>
         </Flex>
-      </Flex>
-      <Footer />
+        <Footer />
+   
     </div>
   );
 };
