1e448cd286a1ba819067d09d3219fadcc8c77c73
SagarPradhan613 <karanpradhan42@gmail.com>
2024-03-24 23:56:04 +0530

    main

--- src/app/airdrop/page.jsx
+++ src/app/airdrop/page.jsx
@@ -420,7 +420,7 @@ const Airdrop = () => {
 
                     {/* footer */}
 
-                    <div className="lg:flex relative z-50  hidden footer-resp-margin -mt-10 items-center w-full justify-end gap-6">
+                    <div className="lg:flex relative z-50  hidden footer-resp-margin -mt-16 items-center w-full justify-end gap-6">
                         <div>
                             <p className="text-white responsive-social-text-hide text-xs lg:font-medium lg:text-base opacity-60">Follow on our socials</p>
                         </div>
