55824f8528f9130e13b87e507215072f39c8aa93
SagarPradhan613 <karanpradhan42@gmail.com>
2024-03-28 01:00:36 +0530

    css

--- src/view/Footer.jsx
+++ src/view/Footer.jsx
@@ -1,3 +1,88 @@
+// import React from "react";
+// import styled from "styled-components";
+// import Flex from "../components/Flex";
+// import Text from "../components/Text";
+// import { COLORS } from '../utils/colors';
+// import useIsMobile from "../hooks/useIsMobile";
+// import { FaTelegramPlane } from "react-icons/fa";
+// import { FaXTwitter } from "react-icons/fa6";
+
+// const HeaderWrapper = styled.div`
+//   width: 100%;
+//   height: 50px;
+//   position: relative;
+//   margin-top: 20px;
+//   @media screen and (max-width: 520px) {
+//     height: 80px;
+//   }
+// `;
+
+// const BgWrapp = styled.div`
+//   position: absolute;
+//   bottom: 0px;
+//   right: 0px;
+// `;
+
+// const Background = styled.div`
+//   width: 490px;
+//   height: 92px;
+//   position: relative;
+//   @media screen and (max-width: 520px) {
+//     width: 350px;
+//     height: 65px;
+//   }
+// `;
+
+// const IconWrapper = styled.div`
+//   position: absolute;
+//   top: 75%;
+//   left: 50%;
+//   transform: translate(-50%, -50%);
+//   @media screen and (max-width: 520px) {
+//     left: 55%;
+//   }
+// `;
+
+// const Icon = styled.a`
+//   color: ${COLORS.white};
+//   cursor: pointer;
+//   transition: transform 1s ease;
+//   &:hover {
+//     color: ${COLORS.blue};
+//   }
+// `;
+
+// const Footer = () => {
+//   const isMobile = useIsMobile();
+//   return (
+//     <div className="w-full h-[50px] relative mt-[20px] footerheaderwrapper">
+//       <div className="absolute bottom-0 right-0">
+//         <div className="w-[490px] h-[92px] relative footerbackground">
+//           <img src={"/Images/Bottom.png"} width={"100%"} />
+//           <div className="absolute top-[75%] left-[50%] footericonwrapper">
+//             <div className="flex justify-between w-full" justify={"space-between"} width={"100%"}>
+//               <p className="text-[#ffffff9c] text-xl" color={COLORS.light} overflow={true} size={"20px"}>
+//                 Follow on our socials
+//               </p>
+//               <div className="flex">
+//                 <div className="icon" href="https://twitter.com/NexusLaunchpad" target="_blank">
+//                   <FaXTwitter />
+//                 </div>
+//                 <div className="icon" href="https://t.me/NexusLaunchpad" target="_blank">
+//                   <FaTelegramPlane />
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Footer;
+
+
 import React from "react";
 import styled from "styled-components";
 import Flex from "../components/Flex";
